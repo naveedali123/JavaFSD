4d3ce84c883a3cdeba09e92d17d7c05ca85e4bb7
naveedali123 <naveedalimphasi@ip-172-31-30-74.ec2.internal>
2022-02-25 15:40:08 +0000

    new changes

---
+++ Programmin_the_Threads_Creation/MyThread.java
@@ -0,0 +1,15 @@
+package Programmin_the_Threads_Creation;
+
+public class MyThread extends Thread
+{
+ 	public void run()
+ 	{
+  		System.out.println("concurrent thread started running..");
+}
+ 	public static void main( String args[] )
+ 	{
+  		MyThread mt = new  MyThread();
+  		mt.start();
+ 	}
+}
+
